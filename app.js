b5a86371e2ccaea4daa190518482c3ec12d38a92
Code Nation 13 <codenation@CN-13.local>
2019-04-02 13:52:03 +0100

    clock update

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
+function runClock() {
+  let now = new Date();
+
+  let hour = now.getHours() % 12;
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+  let ms = now.getMilliseconds();
+
+  let clock = document.querySelector("div.clock");
+  let hourhand = clock.querySelector("div.hour");
+  let minhand = clock.querySelector("div.minute");
+  let sechand = clock.querySelector("div.second");
+
+  let hourRotation = 30 * hour;
+  let minRotation = 6 * min + 0.1 * sec;
+  let secRotation = 6 * sec + 0.006 * ms;
+
+  hourhand.style.transform = "rotate(" + hourRotation + "deg)";
+  minhand.style.transform = "rotate(" + minRotation + "deg)";
+  sechand.style.transform = "rotate(" + secRotation + "deg)";
+
+  requestAnimationFrame(runClock);
+}
+
+runClock();
